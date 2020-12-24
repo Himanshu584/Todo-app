b593e4170d50a6d57a7a16dab61c002cedfcfac2
Himanshu Sharma <Hsharma98765@gmail.com>
2020-12-24 21:18:42 +0530

    added input addition functionality

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import './App.css';
 function App() {
 
   const [todos, setTodos] = useState(['hello','world']);
+  const [input, setInput] = useState('');
+  const addTodo = (e) => {
+    setTodos([...todos, input]);
+    e.preventDefault();
+    setInput('');
+  } 
 
   return (
     <div className="App">
       <h1>TODO-APP</h1>
-      <input />
-      <button>Add Todo</button>
+      <form>
+        <input value={input} onChange= {e => setInput(e.target.value)}/>
+        <button type='submit' onClick={addTodo}>Add Todo</button>
+      </form>
+
       <ul>
         {todos.map(todo => (
           <li>{todo}</li>
